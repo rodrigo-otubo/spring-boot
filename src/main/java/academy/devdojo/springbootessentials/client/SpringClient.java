315b99225eb396ddf99069d89887b238836f26fa
Rodrigo Otubo <rodrigo.otubo@gmail.com>
2021-03-23 21:40:35 -0300

    Exchange, retornando uma lista de objetos

--- src/main/java/academy/devdojo/springbootessentials/client/SpringClient.java
+++ src/main/java/academy/devdojo/springbootessentials/client/SpringClient.java
@@ -2,8 +2,13 @@ package academy.devdojo.springbootessentials.client;
 
 import academy.devdojo.springbootessentials.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -13,7 +18,16 @@ public class SpringClient {
         log.info(entity);
 
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 3);
-
         log.info(object);
+
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+        log.info(Arrays.toString(animes));
+
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange(
+                "http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {});
+        log.info(exchange.getBody());
     }
 }
